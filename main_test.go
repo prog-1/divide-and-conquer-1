0072340e78707cf64598c73e9e16f820c6565622
Richard Shashalevich <richardsasalevic@gmail.com>
2022-11-16 21:12:16 +0200

    addedsome more tests

--- main_test.go
+++ main_test.go
@@ -9,6 +9,8 @@ func TestMaxSumOfSubArray(t *testing.T) {
 	}{
 		{[]int{-1, 1, 2, -1}, 3},
 		{[]int{1, -1, -1, 2}, 2},
+		{[]int{1, 2, -8, 1, 3, 9, -2, 1}, 13},
+		{[]int{1, 0, 1}, 2},
 	} {
 		if got := maxSumOfSubArray(tc.input); tc.want != got {
 			t.Errorf("maxSumofArray(%v) = %v, want = %v", tc.input, got, tc.want)
